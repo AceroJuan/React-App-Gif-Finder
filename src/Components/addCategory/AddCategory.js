e2bfa311b18b88a7c2b274f49378c1a43ebb4696
Juan Acero <dev.acerojuan@gmail.com>
2021-06-17 06:37:41 -0500

    gifGrid component created, render images on grid

--- src/Components/addCategory/AddCategory.js
+++ src/Components/addCategory/AddCategory.js
@@ -3,16 +3,18 @@ import PropTypes from "prop-types";
 
 const AddCategory = ({ setCategories }) => {
   const [inputValue, setInputValue] = useState("");
+
   const handleInputValue = (e) => {
     setInputValue(e.target.value);
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (inputValue.trim().length > 3) {
       setCategories((category) => [...category, inputValue]);
-      setInputValue("");
     }
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" value={inputValue} onChange={handleInputValue} />
--- src/Components/gifGrid/GifGrid.js
+++ src/Components/gifGrid/GifGrid.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import GifGridItem from "../gifGridItem/GifGridItem";
 
 const GifGrid = ({ category }) => {
-  const [count, setCount] = useState(0);
-
+  const [images, setImages] = useState([]);
   useEffect(() => {
     getGifs();
   }, []);
+
   const getGifs = async () => {
     const url =
-      "https://api.giphy.com/v1/gifs/search?q=Goku&limit=10&api_key=1q8bsMLFhZWuVmX3BpYp4CJw8RJqIyKR";
+      "https://api.giphy.com/v1/gifs/search?q=rick%26morty&limit=10&api_key=1q8bsMLFhZWuVmX3BpYp4CJw8RJqIyKR";
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map((img) => {
@@ -20,13 +21,19 @@ const GifGrid = ({ category }) => {
       };
     });
     console.log(gifs);
+    setImages(gifs);
   };
 
   return (
     <>
       <h3>{category}</h3>
-      <h3>{count}</h3>
-      <button onClick={() => setCount(count + 1)}>mas</button>
+      <div>
+        <ul className="card-grid">
+          {images.map((img) => (
+            <GifGridItem key={img.id} {...img} />
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
